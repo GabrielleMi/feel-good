a6a2b35565d8f0d2b5108e476ec70025509b91e7
Gabrielle <gabrielle.mi@hotmail.com>
2024-11-05 20:09:18 -0500

    test(integrations/csp): trigger build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,21 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-// import partytown from '@astrojs/partytown';
+import partytown from '@astrojs/partytown';
 import { csp } from './src/lib/csp';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
     tailwind(),
-    // partytown(),
+    partytown(),
     csp({
       output: 'meta',
       policies: {
         'default-src': ["'self'"],
         'img-src': ["'self'", 'https://images.ctfassets.net'],
         'style-src': ["'self'", "https://fonts.googleapis.com"],
+        'font-src': ["'self'", "https://fonts.gstatic.com"],
       }
     })
   ],
